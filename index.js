e9aff9976345a952741a040ac29390f005e3d09d
Aimkeys Kelvin Mwaura <kelvin.ngugi@student.moringaschool.com>
2022-06-11 01:58:14 +0300

    commit tree 6b0c4404df99ead9e598783b1b25483320fe14ba

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+function findMatching(arrayOfDrivers, string)
+{
+    //filter returns an array
+   let match = arrayOfDrivers.filter(element => element.toUpperCase() === string.toUpperCase())
+   return match
+}
+function fuzzyMatch(arrayOfDrivers,string)
+{
+    return arrayOfDrivers.filter(item=>item[0]===string[0])
+}
+function matchName(arrayOfDrivers, string)
+{
+    return arrayOfDrivers.filter(item =>item.name===string)
+}
+//console.log("saMMy".toUpperCase())
+//console.log(findMatching(drivers,"saMmy"))
